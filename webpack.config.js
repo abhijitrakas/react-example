a1f8633180633eb196e18cf45a15c7887f043ea1
Abhijit Rakas <abhijit.rakas@rtcamp.com>
2022-01-27 15:27:55 +0530

    Add token in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,8 @@ module.exports = {
     },
     plugins: [
         new PacktrackerPlugin({
-          project_token: process.env.PT_PROJECT_TOKEN,
-          upload: !!process.env.PT_PROJECT_TOKEN,
+          project_token: '1a3d4ba8-2ca6-447f-90b9-2b2a1b97b0ab',
+          upload: true,
           fail_build: true
         })
     ]
