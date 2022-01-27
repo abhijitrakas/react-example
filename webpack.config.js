c49a25189e889744813bce35c9350bbc267ca925
Abhijit Rakas <abhijit.rakas@rtcamp.com>
2022-01-27 14:41:17 +0530

    Try pack tracker for performance budget

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
 const webpack = require('webpack');
 const path = require('path');
+const PacktrackerPlugin = require('@packtracker/webpack-plugin')
 
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: __dirname,
-        filename: 'bundel.js'
+        filename: 'build/bundel.js'
     },
     module: {
         rules: [
             {'test': /\.js$/, 'use': 'babel-loader'},
         ]
-    }
+    },
+    plugins: [
+        new PacktrackerPlugin({
+          project_token: process.env.PT_PROJECT_TOKEN,
+          upload: !!process.env.PT_PROJECT_TOKEN,
+          fail_build: true
+        })
+    ]
 };
